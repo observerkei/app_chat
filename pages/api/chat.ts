642be1d684aab0a49917ad5865d950ab3fefac86
observerkei <observerkei@foxmail.com>
2024-03-04 16:10:04 +0800

    style: set tiktoken static model

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -3,15 +3,21 @@ import { OpenAIError, OpenAIStream } from '@/utils/server';
 
 import { ChatBody, Message } from '@/types/chat';
 
-// @ts-expect-error
-import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
 
 export const config = {
   runtime: 'edge',
 };
+
+async function fetchWasm(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch Wasm file: ${response.statusText}`);
+  }
+  const wasmBytes = await response.arrayBuffer();
+  return wasmBytes;
+}
 
 const handler = async (req: Request): Promise<Response> => {
   console.log('try chat...');
@@ -22,6 +28,8 @@ const handler = async (req: Request): Promise<Response> => {
     console.log('chat get api Host: ', apiHost)
     console.log('chat model: ', JSON.stringify(model))
     
+    const wasm = await fetchWasm('http://localhost:2300/tiktoken_bg.wasm');
+
     await init((imports) => WebAssembly.instantiate(wasm, imports));
     const encoding = new Tiktoken(
       tiktokenModel.bpe_ranks,

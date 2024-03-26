6ccd34c9af77868f97116e8da6591264ea4d23d8
observerkei <observerkei@foxmail.com>
2024-03-26 22:59:59 +0800

    style: set chat input portrait to mb-5

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -310,7 +310,7 @@ export const ChatInput = ({
             ShowRegenerateButton(selectedConversation?.model)
           )}
 
-        <div className="relative portrait:mb-10 landscape:mb-3 mx-5 flex w-full flex-grow flex-col rounded-md border border-black/10 bg-white shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:border-gray-900/50 dark:bg-[#161616] dark:text-white dark:shadow-[0_0_15px_rgba(0,0,0,0.10)] sm:mx-4">
+        <div className="relative portrait:mb-5 landscape:mb-3 mx-5 flex w-full flex-grow flex-col rounded-md border border-black/10 bg-white shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:border-gray-900/50 dark:bg-[#161616] dark:text-white dark:shadow-[0_0_15px_rgba(0,0,0,0.10)] sm:mx-4">
           <button
             className="absolute left-2 top-2 rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
             onClick={() => setShowPluginSelect(!showPluginSelect)}

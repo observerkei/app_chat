53547a54257946b00c1a274f33d52e194923c6f1
observerkei <observerkei@foxmail.com>
2024-11-07 00:11:22 +0800

    feat: add android build.

---
+++ android/app/src/main/java/com/observerkei/chatbot/MainActivity.java
@@ -0,0 +1,5 @@
+package com.observerkei.chatbot;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.observerkei.chatbot',
+  appName: 'ai-chatbot-starter',
+  webDir: 'out'
+};
+
+export default config;

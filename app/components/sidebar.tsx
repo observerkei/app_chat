2be9fdb5328173cf38386511b04e4dbf1f3fd473
observerkei <observerkei@foxmail.com>
2025-02-08 14:41:42 +0800

    refactor: Use a custom color scheme

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import styles from "./home.module.scss";
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/github.svg";
-import ChatGptIcon from "../icons/chatgpt.svg";
+import ChatGptIcon from "@/custom/app/icons/chatgpt.svg";
 import AddIcon from "../icons/add.svg";
 import DeleteIcon from "../icons/delete.svg";
 import MaskIcon from "../icons/mask.svg";
